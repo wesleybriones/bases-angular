245e46ab6ba0c977a26f4129906fda831229ab11
wesleybriones <wesgbrio@espol.edu.ec>
2021-08-09 15:55:06 -0500

    bases de angular posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //AboutComponent,
+    //ContactComponent,
+    //HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  mensajes: any;
+
+  constructor( private dataService: DataService) { }
+
+  ngOnInit(): void {
+    //console.log('INIT!');
+
+    this.mensajes = this.dataService.getPosts();
+
+
+    //  .subscribe( (posts:any) => {
+    //    console.log(posts);
+    //   this.mensajes = posts;
+    //  });
+  }
+
+}
