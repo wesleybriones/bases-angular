4b4469cbd04c06062e90601162abbf4fa8e1a384
wesleybriones <wesgbrio@espol.edu.ec>
2021-08-09 16:12:18 -0500

    input desde el hijo

---
+++ src/app/pages/posts/post/post.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() mensaje:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
